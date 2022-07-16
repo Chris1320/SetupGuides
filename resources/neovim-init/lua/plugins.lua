df74e027cf2adad50c1bc35fdf1a47557dcc8b72
Chris1320 <chris1320is@protonmail.com>
2022-07-16 16:12:39 +0800

    [neovim.md] Explicitly set "show dotfiles" in nvim-tree.
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- resources/neovim-init/lua/plugins.lua
+++ resources/neovim-init/lua/plugins.lua
@@ -209,7 +209,13 @@ end
 
 local function setupNvimTree()
     local nvim_tree = require("nvim-tree")
-    nvim_tree.setup()
+    nvim_tree.setup(
+        {
+            filters = {
+                dotfiles = false
+            }
+        }
+    )
 end
 
 local function setupLspConfig()
